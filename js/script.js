db7db5e78d7d4a551cd814b4a0e70cf654a9b689
diannedaian <142431216+diannedaian@users.noreply.github.com>
2025-08-11 11:41:46 -0400

    fixed links

--- js/script.js
+++ js/script.js
@@ -1,118 +1,122 @@
-(function($) {
+(function ($) {
   "use strict";
 
   // init Isotope
-  var initIsotope = function() {
-    
-    $('.grid').each(function(){
-
+  var initIsotope = function () {
+    $(".grid").each(function () {
       // $('.grid').imagesLoaded( function() {
-        // images have loaded
-        var $buttonGroup = $( '.button-group' );
-        var $checked = $buttonGroup.find('.is-checked');
-        var filterValue = $checked.attr('data-filter');
-  
-        var $grid = $('.grid').isotope({
-          itemSelector: '.portfolio-item',
-          // layoutMode: 'fitRows',
-          filter: filterValue
+      // images have loaded
+      var $buttonGroup = $(".button-group");
+      var $checked = $buttonGroup.find(".is-checked");
+      var filterValue = $checked.attr("data-filter");
+
+      var $grid = $(".grid").isotope({
+        itemSelector: ".portfolio-item",
+        // layoutMode: 'fitRows',
+        filter: filterValue,
+        initLayout: false, // Prevent initial layout
+      });
+
+      // Show grid after Isotope is initialized
+      $grid.addClass("isotope-ready");
+
+      // Trigger layout after a short delay to ensure smooth transition
+      setTimeout(function () {
+        $grid.isotope("layout");
+      }, 100);
+
+      // bind filter button click
+      $(".button-group").on("click", "a", function (e) {
+        e.preventDefault();
+        filterValue = $(this).attr("data-filter");
+        $grid.isotope({ filter: filterValue });
+      });
+
+      // change is-checked class on buttons
+      $(".button-group").each(function (i, buttonGroup) {
+        $buttonGroup.on("click", "a", function () {
+          $buttonGroup.find(".is-checked").removeClass("is-checked");
+          $(this).addClass("is-checked");
         });
-    
-        // bind filter button click
-        $('.button-group').on( 'click', 'a', function(e) {
-          e.preventDefault();
-          filterValue = $( this ).attr('data-filter');
-          $grid.isotope({ filter: filterValue });
-        });
-    
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-          $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-          });
-        });
+      });
       // });
-
     });
-  }
+  };
 
-  var initScrollNav = function() {
+  var initScrollNav = function () {
     var scroll = $(window).scrollTop();
-    console
+    console;
 
     if (scroll >= 200) {
-      $('#header.fixed-top').addClass("bg-white");
-    }else{
-      $('#header.fixed-top').removeClass("bg-white");
+      $("#header.fixed-top").addClass("bg-white");
+    } else {
+      $("#header.fixed-top").removeClass("bg-white");
     }
-  }
+  };
 
   // init Chocolat light box
-  var initChocolat = function() {
-    Chocolat(document.querySelectorAll('.image-link'), {
-      imageSize: 'contain',
+  var initChocolat = function () {
+    Chocolat(document.querySelectorAll(".image-link"), {
+      imageSize: "contain",
       loop: true,
-    })
-  }
+    });
+  };
 
   // ------------------------------------------------------------------------------ //
-	// Overlay Menu Navigation
-	// ------------------------------------------------------------------------------ //
-	var overlayMenu = function () {
+  // Overlay Menu Navigation
+  // ------------------------------------------------------------------------------ //
+  var overlayMenu = function () {
+    if (!$(".nav-overlay").length) {
+      return false;
+    }
 
-		if(!$('.nav-overlay').length) {
-		  return false;
-		}
+    var body = undefined;
+    var menu = undefined;
+    var menu_close = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+      body = document.querySelector("body");
+      menu = document.querySelector("#menu-btn");
+      menu_close = document.querySelector("#menu-btn-close");
+      menuItems = document.querySelectorAll(".nav__list-item");
+      applyListeners();
+    };
+    var applyListeners = function applyListeners() {
+      menu.addEventListener("click", function () {
+        return toggleClass(body, "nav-active");
+      });
+      menu_close.addEventListener("click", function () {
+        return toggleClass(body, "nav-active");
+      });
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+      if (element.classList.contains(stringClass))
+        element.classList.remove(stringClass);
+      else element.classList.add(stringClass);
+    };
+    init();
+  };
 
-		var body = undefined;
-		var menu = undefined;
-		var menu_close = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-		  body = document.querySelector('body');
-		  menu = document.querySelector('#menu-btn');
-		  menu_close = document.querySelector('#menu-btn-close');
-		  menuItems = document.querySelectorAll('.nav__list-item');
-		  applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-		  menu.addEventListener('click', function () {
-		    return toggleClass(body, 'nav-active');
-		  });
-		  menu_close.addEventListener('click', function () {
-		    return toggleClass(body, 'nav-active');
-		  });
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-		  if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}
-
-  $(document).ready(function(){
+  $(document).ready(function () {
     initScrollNav();
     overlayMenu();
     initChocolat();
-    
+
     AOS.init({
       duration: 1000,
-      once: true
-    })
+      once: true,
+    });
 
-    $('#btn-menu').click(function(e){
+    $("#btn-menu").click(function (e) {
       e.preventDefault();
-      $('.sidebar-menu').toggleClass('open');
-    })
+      $(".sidebar-menu").toggleClass("open");
+    });
 
+    // Initialize Isotope earlier for better performance
+    initIsotope();
   }); // End of a document
 
-  $(window).load(function () {
-    initIsotope();
-  });
-
-  $(window).scroll(function() {    
+  $(window).scroll(function () {
     initScrollNav();
-  }); 
-
+  });
 })(jQuery);
